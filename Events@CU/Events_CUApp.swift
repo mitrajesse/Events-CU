4f9547368a93861d8540de7d3037d33e5a729606
mitrajesse <mitrajesse@gmail.com>
2024-09-12 17:26:17 -0400

    Initial Commit

---
+++ Events@CU/Events_CUApp.swift
@@ -0,0 +1,17 @@
+//
+//  Events_CUApp.swift
+//  Events@CU
+//
+//  Created by Jesse Mitra on 9/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Events_CUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
