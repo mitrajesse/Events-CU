cdad15de6cab996524931ee7c0438ac821b9977c
mitrajesse <mitrajesse@gmail.com>
2024-12-12 21:10:18 -0500

    fixed reset password

--- Views/ResetPasswordView.swift
+++ Views/ResetPasswordView.swift
@@ -1,60 +1,46 @@
-//
-//  ResetPasswordView.swift
-//  Events@CU
-//
-//  Created by Jesse Mitra and Abe Howder on 12/12/24.
-//
-
 import SwiftUI
 
 struct ResetPasswordView: View {
     @ObservedObject var viewModel: ProfileViewModel
-    @Environment(\.presentationMode) var presentationMode
     
     var body: some View {
-        ZStack {
-            Color(UIColor(red: 0/255, green: 56/255, blue: 101/255, alpha: 1))
-                .ignoresSafeArea()
-            
-            VStack(spacing: 20) {
-                Text("Reset Password")
-                    .font(.largeTitle)
-                    .bold()
-                    .foregroundColor(Color(UIColor(red: 252/255, green: 183/255, blue: 22/255, alpha: 1)))
-                    .padding()
+        NavigationView {
+            ZStack {
+                Color(UIColor(red: 0/255, green: 56/255, blue: 101/255, alpha: 1))
+                    .ignoresSafeArea()
                 
-                TextField("Enter your email", text: $viewModel.resetPasswordEmail)
-                    .textFieldStyle(AuthTextFieldStyle())
-                    .autocapitalization(.none)
-                    .keyboardType(.emailAddress)
-                    .accentColor(.black)
-                    .colorScheme(.light)
-                
-                Button(action: {
-                    viewModel.resetPassword()
-                    presentationMode.wrappedValue.dismiss()
-                }) {
-                    Text("Send Reset Email")
+                VStack(spacing: 20) {
+                    Spacer() // Push content higher
+                    
+                    Text("Reset Password")
+                        .font(.largeTitle)
                         .bold()
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                        .background(Color(UIColor(red: 252/255, green: 183/255, blue: 22/255, alpha: 1)))
-                        .foregroundColor(.black)
-                        .cornerRadius(10)
+                        .foregroundColor(Color(UIColor(red: 252/255, green: 183/255, blue: 22/255, alpha: 1)))
+                        .padding(.bottom, 20) // Add some bottom padding
+                    
+                    TextField("Enter your email", text: $viewModel.resetPasswordEmail)
+                        .textFieldStyle(AuthTextFieldStyle())
+                        .autocapitalization(.none)
+                        .keyboardType(.emailAddress)
+                        .accentColor(.black)
+                        .colorScheme(.light)
+                    
+                    Button(action: {
+                        viewModel.resetPassword()
+                    }) {
+                        Text("Send Reset Email")
+                            .bold()
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(Color(UIColor(red: 252/255, green: 183/255, blue: 22/255, alpha: 1)))
+                            .foregroundColor(.black)
+                            .cornerRadius(10)
+                    }
+                    
+                    Spacer(minLength: 462.5) // Add some spacing at the bottom
                 }
-                
-                Button(action: { presentationMode.wrappedValue.dismiss() }) {
-                    Text("Cancel")
-                        .bold()
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                        .background(Color.red)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
-                }
+                .padding()
             }
-            .padding()
         }
-        .navigationBarHidden(true)
     }
 }
